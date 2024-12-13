89db4f5b65e5b9b05d608693552f4581f0da35d7
Naz Rejepov <nazar.rejepov@qa.com>
2024-12-13 11:21:34 +0000

    add bootstrap

---
+++ bootstrap.ps1
@@ -0,0 +1,46 @@
+function Set-LabArtifacts {
+    $ProgressPreference = 'SilentlyContinue'
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    # Download the image to Windows Temp folder
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cloudacademy/azure-lab-provisioners/master/azure-storage/qaimage.png" -OutFile "C:\Windows\Temp\qaimage.png"
+
+    # Download the task XML. Using XML is preferred to minimize potential issues.
+    $taskXml = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/nrejepov/test-public-scripts/refs/heads/main/task.xml" -UseBasicParsing | Select-Object -ExpandProperty Content
+
+    # Register the scheduled task using the XML definition
+    Register-ScheduledTask -TaskName "CopyImage" -Xml $taskXml -Force
+}
+
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+function Enable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 1 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1 -Force
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been enabled." -ForegroundColor Green
+}
+
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000 -Force
+    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+}
+
+# Disable Windows Defender real-time monitoring
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Disable Windows update
+Stop-Service -NoWait -displayname "Windows Update"
+
+Set-LabArtifacts
+Disable-UserAccessControl
+Disable-InternetExplorerESC
